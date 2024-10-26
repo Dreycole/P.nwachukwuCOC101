cb149480aaca2d25e5275b60ff638563931a37e8
Dreycole <pascalchidox18@gmail.com>
2024-10-26 04:24:27 +0100

    add week 2

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main() {
+	println!();// prints just a
+	println!("hello");//prints
+	println!("format{} arguments", "some");//prints format some arguments
+	println!("My name is {}. I  am the best progamming student in stream 1", "Chidozie Nwachukwu Pascal");
+}
